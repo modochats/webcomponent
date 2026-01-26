fcb339ad6e7330295478c7e8d300581736debc3c
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2026-01-26 21:41:28 +0330

    chore: update build and release workflow, add semantic release configuration, and manage versioning

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
+import {VERSION} from "./version.js";
 const getEnvironment = () => {
   // Check for browser global variable
   if (typeof window !== "undefined" && (window as any).ENVIRONMENT) {
@@ -15,7 +16,6 @@ const isDev = getEnvironment() === "DEV";
 const isProd = getEnvironment() === "PROD";
 const BASE_API_URL = isDev ? "https://dev-api.modochats.com" : "https://api.modochats.com";
 const BASE_WEBSOCKET_URL = isDev ? "wss://dev-api.modochats.com/ws" : "wss://api.modochats.com/ws";
-const VERSION = "0.51";
 const BASE_STORAGE_URL = "https://modochats.s3.ir-thr-at1.arvanstorage.ir";
 const NEW_MESSAGE_AUDIO_URL = `${BASE_STORAGE_URL}/new-message.mp3`;
 export {BASE_API_URL, BASE_WEBSOCKET_URL, VERSION, isDev, isProd, getEnvironment, NEW_MESSAGE_AUDIO_URL, BASE_STORAGE_URL};

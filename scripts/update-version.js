81213d829bad7b750333fe45c7129afd4fd80950
Enlil <116581871+Abolfazl2049@users.noreply.github.com>
2026-01-26 22:25:34 +0330

    fix: update version update script to use async file writing

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import {promises as fs} from "fs";
 import path from "path";
+import {fileURLToPath} from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const packageJson = JSON.parse(await fs.readFile(path.join(__dirname, "../package.json"), "utf8"));
 const version = packageJson.version;
 
 const constantsPath = path.join(__dirname, "../src/constants/version.ts");
 const constantsContent = `const VERSION = "${version}";\nexport {VERSION};\n`;
 
-fs.writeFileSync(constantsPath, constantsContent);
+fs.writeFile(constantsPath, constantsContent);
 console.log(`Updated VERSION to ${version} in version.ts`);
